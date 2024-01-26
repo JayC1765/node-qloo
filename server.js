ff77c5d1130b7f9c22308d5020b34218c5a75663
Jason Chan <jason.chann91@gmail.com>
2024-01-26 16:16:26 -0500

    implemented process-data endpoint

--- server.js
+++ server.js
@@ -1,27 +1,89 @@
 const express = require('express');
 const axios = require('axios');
+const redis = require('redis');
 const app = express();
 const PORT = 3000;
+const usersURI = 'https://jsonplaceholder.typicode.com/users';
+const albumsURI = 'https://jsonplaceholder.typicode.com/albums';
+const { getOrSetCache } = require('./util');
+
+const redisClient = redis.createClient();
 
 // Express middleware to parse requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // fetch-data API endpoint
-app.get('/fetch-data', (req, res) => {
+app.get('/fetch-data', async (req, res) => {
   try {
-    axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
-      const { data } = response;
-      return res.status(200).json(data);
+    // With helper function, get the users from cache otherwise fetch from API
+    const users = await getOrSetCache(redisClient, 'allUsers', async () => {
+      const { data } = await axios.get(usersURI);
+      return data;
     });
+
+    return res.status(200).json(users);
   } catch (err) {
-    if (err) {
-      console.error(`Error fetching data, ${err}`);
-    }
+    console.error(`Error fetching data, ${err}`);
+    return res.status(500).json({ message: 'Error with fetching data' });
   }
 });
 
-// catch-all route handler for any requests to an unknown route
+/*
+process-data API endpoint: fetches the most updated data for all users and albums. It will then associate all albums
+to the respective user, store each user in Redis for future queries, and returns the update user object to the client.
+*/
+app.get('/process-data', async (req, res) => {
+  try {
+    // Fetch all updated users and albums
+    const { data: allUsers } = await axios.get(usersURI);
+    const { data: allAlbums } = await axios.get(albumsURI);
+    const allUsersArr = [];
+    const usersHash = {};
+
+    // Store all users in a hashmap
+    for (const users of allUsers) {
+      const userId = `userId${users.id}`;
+      delete users.id;
+      usersHash[userId] = users;
+    }
+
+    // Connect all albums with the respective users
+    for (const album of allAlbums) {
+      const userId = `userId${album.userId}`;
+      delete album.userId;
+      if (userId in usersHash) {
+        usersHash[userId]['albums'] = usersHash[userId]['albums'] || [];
+        usersHash[userId]['albums'].push(album);
+      } else {
+        console.log('A user could not be found for this album');
+      }
+    }
+
+    /* 
+    Store each updated user with their albums into Redis for future access. Return an array of
+    all updated users to the client.
+    */
+    for (const [userId, userDetails] of Object.entries(usersHash)) {
+      allUsersArr.push({ [userId]: userDetails });
+
+      redisClient.set(userId, JSON.stringify(userDetails), (err, result) => {
+        if (err) {
+          console.error(`Error storing ${userId}`);
+        } else {
+          console.log(`Successfully store ${userId}`);
+        }
+      });
+    }
+
+    return res.status(200).json(allUsersArr);
+  } catch (err) {
+    console.error(`Error processing data, ${err}`);
+    return res.status(500).json({ message: 'Error with processing data' });
+  }
+});
+
+// Catch-all route handler for any requests to an unknown route
 app.use((req, res) =>
   res.status(404).send("This is not the page you're looking for...")
 );
@@ -35,7 +97,17 @@ app.listen(PORT, (err) => {
   }
 });
 
-// event listener for server errors
+// Event listener for server errors
 app.on('error', (err) => {
   console.error(`Server encountered an error: ${err}`);
 });
+
+redisClient
+  .connect()
+  .then(() => {
+    console.log('Connected to Redis');
+    redisClient.on('error', (err) => console.log('Redis Client Error: ', err));
+  })
+  .catch((err) => {
+    console.error('Error connecting to Redis:', err);
+  });
--- util.js
+++ util.js
@@ -1,3 +1,7 @@
+/* 
+Helper function to get data from cache, if available. Otherwise, invoke callback function
+and cache the result in Redis with a default expiration of 1 hour
+*/
 const getOrSetCache = async (redisClient, key, cb, default_exp = 3600) => {
   try {
     const data = await redisClient.get(key);
@@ -5,8 +9,7 @@ const getOrSetCache = async (redisClient, key, cb, default_exp = 3600) => {
     if (data) {
       return JSON.parse(data);
     } else {
-      console.log('Cache missed');
-      const fetchedData = cb();
+      const fetchedData = await cb();
       await redisClient.setEx(key, default_exp, JSON.stringify(fetchedData));
       return fetchedData;
     }
