898a5b52fa9af5d4321983d23b4a4034df6d0c5f
Jason Chan <jason.chann91@gmail.com>
2024-01-26 13:53:49 -0500

    boilerplate to start up server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(`Error while connecting to server: ${err}`);
+    process.exit(1);
+  } else {
+    console.log(`Listening on PORT ${PORT}`);
+  }
+});
