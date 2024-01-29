1a696ba523867749e618c71165b8af0bd631abea
Jason Chan <jason.chann91@gmail.com>
2024-01-29 10:42:08 -0500

    fixed error handling on getOrSetCache helper function

--- util.js
+++ util.js
@@ -2,21 +2,24 @@
 Helper function to get data from cache, if available. Otherwise, invoke callback function
 and cache the result in Redis with a default expiration of 1 hour
 */
-const getOrSetCache = async (redisClient, key, cb, default_exp = 3600) => {
-  try {
-    const data = await redisClient.get(key);
-
-    if (data) {
-      return JSON.parse(data);
-    } else {
-      const fetchedData = await cb();
-      await redisClient.setEx(key, default_exp, JSON.stringify(fetchedData));
-      return fetchedData;
-    }
-  } catch (err) {
-    console.error('Error inside getOrSetCache: ', err);
-    throw err;
-  }
+const getOrSetCache = (redisClient, key, cb, default_exp = 3600) => {
+  return new Promise((resolve, reject) => {
+    redisClient
+      .get(key)
+      .then(async (response) => {
+        if (response) {
+          return resolve(JSON.parse(response));
+        } else {
+          const fetchedData = await cb();
+          redisClient.setEx(key, default_exp, JSON.stringify(fetchedData));
+          return resolve(fetchedData);
+        }
+      })
+      .catch((err) => {
+        console.error('Error inside getOrSetCache: ', err);
+        return reject(err);
+      });
+  });
 };
 
 module.exports = {
